5bc81ae3514da3d6c34cf5f53b5e99c456feb12f
Venc03 <vegi.dani1@gmail.com>
2023-03-10 11:03:24 +0100

    formazas

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const lista = ["fel1.jpg", "yes.png", "yes1.png", "yes2.jpg", "yes3.jpg", "yes4.jpg", "yes5.jpg", "yes6.png"];
+const Lista = ["fel1.jpg", "yes.png", "yes1.png", "yes2.jpg", "yes3.jpg", "yes4.jpg", "yes5.jpg", "yes6.png"];
+
 
 window.addEventListener("load", main);
+
 
 function main(){
     kepLista();
@@ -9,60 +11,65 @@ function main(){
     buttons();
 }
 
+
 function kepLista(){
-    const art = document.querySelectorAll("article");
-    for(let i = 0; i < lista.length; i++){
-        const img = document.createElement("img");
-        img.src = lista[i];
-        art[0].appendChild(img);
+    const Art = document.querySelectorAll("article");
+    for(let i = 0; i < Lista.length; i++){
+        const Img = document.createElement("img");
+        Img.src = Lista[i];
+        Art[0].appendChild(Img);
     
-        img.addEventListener("click", function(){
-            const bImg = document.querySelectorAll("section div img")
-            bImg[0].src = lista[i];
+        Img.addEventListener("click", function(){
+            const BImg = document.querySelectorAll("section div img")
+            BImg[0].src = Lista[i];
         })
     } 
 }
 
+
 function section(){
-    const sec = document.querySelectorAll("section");
-    sec[0].innerHTML = "<button class='lbutton'>&#8592</button><div></div><button class='rbutton'>&#8594</button>";
+    const Sec = document.querySelectorAll("section");
+    Sec[0].innerHTML = "<button class='lbutton'>&#8592</button><div></div><button class='rbutton'>&#8594</button>";
 }
 
+
 function kepKiv(){
-    const div = document.querySelectorAll("div");
-    const img = document.createElement("img");
-    const i = rnd();
-    img.src = lista[i];
-    div[0].appendChild(img);
+    const Div = document.querySelectorAll("div");
+    const Img = document.createElement("img");
+    const I = rnd();
+    Img.src = Lista[I];
+    Div[0].appendChild(Img);
     
 }
 
+
 function rnd(){
-    const min = 0;
-    const max = lista.length - 1;
-    const randomN = Math.floor(Math.random() * (max - min + 1)) + min;
+    const Min = 0;
+    const Max = Lista.length - 1;
+    const randomN = Math.floor(Math.random() * (Max - Min + 1)) + Min;
     return randomN;
 }
+
 
 function buttons(){
     const lbutton = document.getElementsByClassName("lbutton")[0];
     const rbutton = document.getElementsByClassName("rbutton")[0]
     let currind = 0
     lbutton.addEventListener("click", function(){
-        const bImg = document.querySelectorAll("section div img")[0]
+        const BImg = document.querySelectorAll("section div Img")[0]
         currind--;
         if (currind < 0){
-            currind = lista.length - 1;
+            currind = Lista.length - 1;
         }
-        bImg.src = lista[currind];
+        BImg.src = Lista[currind];
     })
 
     rbutton.addEventListener("click", function(){
-        const bImg = document.querySelectorAll("section div img")[0]
+        const BImg = document.querySelectorAll("section div Img")[0]
         currind++;
-        if (currind >= lista.length){
+        if (currind >= Lista.length){
             currind = 0;
         }
-            bImg.src = lista[currind];
+            BImg.src = Lista[currind];
     })
 }
